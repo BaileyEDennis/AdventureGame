d78948fd91bbe14c35083a21f940ca4b11fce693
Bailey <bailey.elise.dennis@gmail.com>
2021-02-14 15:37:48 -0600

    first commit

---
+++ AdventureGame/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string CharacterName = "";
+
+            Console.WriteLine("Ruins of Mizzulft");
+            Console.WriteLine("Welcome to ...\n");
+            Console.WriteLine("What would you like to name your Character?");
+            Console.ReadKey();
+        }
+    }
+}
