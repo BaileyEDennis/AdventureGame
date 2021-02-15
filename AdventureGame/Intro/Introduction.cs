ddef61b7f21caa01eb6c925cdb95807afa49094e
Bailey <bailey.elise.dennis@gmail.com>
2021-02-15 15:21:19 -0600

    cleaned up menus and look

--- AdventureGame/Intro/Introduction.cs
+++ AdventureGame/Intro/Introduction.cs
@@ -5,10 +5,14 @@ namespace AdventureGame.Intro
 {
     class Creation
     {
+        public static bool SaveHolder { get; set; }
         public static string CharacterName { get; set; }
-        public static bool Confirm { get; set; }
-        public static string CharacterRace { get; set; } = "";
-        public static bool Confirm2 { get; set; } = false;
+        public static bool ConfirmName { get; set; }
+        public static string CharacterRace { get; set; }
+        public static bool ConfirmRace { get; set; }
+        public static string CharacterClass { get; set; }
+        public static bool ConfirmClass { get; set; }
+        public static bool TutorialCheck { get; set; }
 
         public static void characterCreation()
         {
@@ -19,22 +23,47 @@ namespace AdventureGame.Intro
                     .Color(Color.Red));
 
 
-            AnsiConsole.Markup("[wheat1]Welcome to [red]Morheim[/]. You are the only survivor from an attack on your caravan " +
-                "by bandits who have taken you prisoner. You are being held in a dungeon cell in the ruins. Can you escape " +
-                "and survive the dangers?[/]\n");
+            AnsiConsole.Markup("[red]Morheim[/] [wheat1], once a propserous and wealthy land. Over the ages, and reigns " +
+                "of evil kings, [red]Morheim[/] fell to ruin. Rumors of the [purple]Broken Crown's[/] resting place as well as " +
+                "its guardians caution foolhardy would-be travelers. Are you a fool, or a conqueror?[/]\n");
             Console.WriteLine("\n");
-            AnsiConsole.Markup("[wheat1]Press any key to continue...[/]");
-            Console.WriteLine("\n");
-            Console.ReadKey();
+            
+            //saved game check
+            do
+            {
+                SaveHolder = AnsiConsole.Confirm($"[wheat1]Start New Game?[/]");
+            } while (SaveHolder != true);
+            
+            Console.Clear();
+            var title = new Rule("[fuchsia]Character Creation[/]");
+            title.Centered();
+            AnsiConsole.Render(title);
 
             //character name
             do
             {
                 CharacterName = AnsiConsole.Ask<string>("[wheat1]What is your [green]name[/], adventerer?[/]");
-                Confirm = AnsiConsole.Confirm($"[wheat1]Do you want to be known as [green]{CharacterName}[/]?[/]");
-            } while (Confirm != true);
+                ConfirmName = AnsiConsole.Confirm($"[wheat1]Do you want to be known as [green]{CharacterName}[/]?[/]");
+            } while (ConfirmName != true);
             AnsiConsole.Markup($"[wheat1]You are now known as [green]{CharacterName}[/].[/]\n");
 
+            //character class
+            do
+            {
+                CharacterClass = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                    .Title("[wheat1]What is your [mediumspringgreen]Class[/]?[/]")
+                    .PageSize(10)
+                    .AddChoices(new[]
+                    {
+                        "Warrior", "Wizard", "Bard",
+                        "Rogue", "Ranger",
+                    }));
+                ConfirmClass = AnsiConsole.Confirm($"[wheat1]Do you want to be a [mediumspringgreen]{CharacterClass}[/]?[/]");
+
+            } while (ConfirmClass != true);
+            AnsiConsole.Markup($"[wheat1]You are now a(n) [mediumspringgreen]{CharacterClass}[/].[/]\n");
+            
             //character race
             do
             {
@@ -48,14 +77,15 @@ namespace AdventureGame.Intro
                         "Elf", "Half-Elf", "Dwarf",
                         "Orc", "Half-Orc", "Halfling",
                     }));
-                Confirm2 = AnsiConsole.Confirm($"[wheat1]Do you want to be a [orange1]{CharacterRace}[/]?[/]");
-            } while (Confirm2 != true);
+                ConfirmRace = AnsiConsole.Confirm($"[wheat1]Do you want to be a [orange1]{CharacterRace}[/]?[/]");
+            } while (ConfirmRace != true);
             AnsiConsole.Markup($"[wheat1]You are now a(n) [green]{CharacterRace}[/].[/]\n");
             AnsiConsole.Markup("[wheat1]You have now made your character![/]\n");
-            AnsiConsole.Markup("[wheat1]Press any key to continue...[/]");
-            Console.WriteLine("\n");
-            Console.ReadKey();
-
+            TutorialCheck = AnsiConsole.Confirm($"[wheat1]Do you want to skip the tutorial?[/]");
+            if (TutorialCheck != true)
+            {
+                Tutorial.TutorialTime();
+            }
 
         }
     }
